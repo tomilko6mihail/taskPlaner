46cf79437c4cdde33ffd830cf2e9f844ed2e635d
MihailTomilko <mishatomilko@yandex.ru>
2025-02-08 01:03:04 +0300

    try 17

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -16,11 +16,11 @@
 
 <script>
 
-//import dragula from 'dragula';
+import dragula from 'dragula'; //включать при деплое
 import headerComponent from './components/headerComponent.vue';
 import TaskColumn from './components/taskColumn.vue';
 import ModalWindow from './components/UI/modalWindow.vue';
-//import './lib/dragula.min.js' //включать при локальной разработке и деплое
+//import './lib/dragula.min.js' //включать при локальной разработке
 import store from './store';
 import { mapActions } from 'vuex';
 export default {
